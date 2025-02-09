0552ca6cffcb9e3543d7f9d3ec80395c5826ebe9
Edvid <spacemastr@gmail.com>
2025-02-09 04:22:15 +0100

    milestone worth saving

---
+++ percentage_fetcher_for_bulbapedia.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         Percentage fetcher for bulbapedia
+// @namespace    http://tampermonkey.net/
+// @version      2025-02-09
+// @description  try to take over the world!
+// @author       You
+// @match        https://bulbapedia.bulbagarden.net/wiki/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bulbagarden.net
+// @grant        none
+// ==/UserScript==
+
+
+
+(function() {
+  'use strict';
+  var locations_table_class = "location_table_Obs7o"
+
+  var body = document.querySelector("body")
+  body.style.background = "cyan"
+
+  var game_locations = document.querySelector("#Game_locations")
+  var game_locations_table = game_locations.parentNode.nextSibling.nextSibling
+  game_locations_table.style.borderWidth = "10px"
+  game_locations_table.classList.add(locations_table_class)
+  console.log(game_locations_table)
+  var generation_tables = document.querySelectorAll(`.${locations_table_class}>tbody>tr>td>table`)
+  var generation_table_index = 0
+  console.log(generation_tables)
+  generation_tables.forEach((generation_table) => {
+    var this_generation_table_class = "generation_table_31Lhg_" + generation_table_index
+    generation_table.classList.add(this_generation_table_class)
+
+    var generation_name = document.querySelector(`.${this_generation_table_class}>tbody>tr>th>*`).innerText
+    console.log(generation_name)
+
+    var inner_generation_table_class = "inner_generation_table_W6Wd8_" + generation_table_index
+    var inner_generation_table = document.querySelector(`.${this_generation_table_class}>tbody>tr>td>table`)
+    inner_generation_table.classList.add(inner_generation_table_class)
+
+    var route_set_names = document.querySelectorAll(`.${inner_generation_table_class}>tbody>tr`)
+    var route_sets = document.querySelectorAll(`.${inner_generation_table_class}>tbody>tr>td>table>tbody>tr>td`)
+
+    var route_set_index = 0
+    route_sets.forEach((route_set) => {
+      var game_names = Array.from(route_set_names[route_set_index].querySelectorAll("th>a>span")).map((el) => el.innerText)
+      var routes = Array.from(route_set.querySelectorAll("a")).map((el) => el.innerText)
+
+      console.log(game_names.join(",") + ": " + routes.join(","))
+      route_set_index++
+    })
+    //https://bulbapedia.bulbagarden.net/wiki/Sinnoh_Route_205#Generation_IV
+    generation_table_index++
+  })
+})();
